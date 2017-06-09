9138d2e2552e067f16f2218aede665983aa21c5d
PoissonSoluble <vincent.duplessis14@gmail.com>
2017-06-09 16:19:17 +0200

    docker works

---
+++ reward_0.py
@@ -0,0 +1,28 @@
+import gym
+from gym import spaces
+import numpy as np
+# from os import path
+import snakeoil3_gym as snakeoil3
+import numpy as np
+import copy
+import collections as col
+import os
+import time
+import json
+
+from larva import *
+
+def reward(obs, obs_pre):
+
+	sp = np.array(obs['speedX'])
+
+	progress = sp*np.cos(obs['angle']) - np.abs(sp*np.sin(obs['angle'])) - sp * np.abs(obs['trackPos'])
+	reward = progress
+	
+	#collision detection
+	if obs['damage'] - obs_pre['damage'] > 0:
+		reward = -1
+
+	return reward
+
+
