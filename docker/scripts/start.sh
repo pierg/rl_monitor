5908d09bc213acee095b884e47936aad4c64a78a
PoissonSoluble <vincent.duplessis14@gmail.com>
2017-07-05 10:52:48 +0200

    add reward name in results

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo "Launch TORCS simulation with ddpg.py and LARVA monitoring"
 
+detached=""
+monitor=""
+fn="reward_3_speed"
+
 while getopts ":dm:" opt; do
 	case $opt in
 		d)
-			detached="y"
+			detached="-d"
 		;;
 		m)
 			fn=$OPTARG
+			monitor="-monitor $fn"
 		;;
 		\?)
 			echo "???" >&2
@@ -24,18 +29,7 @@ sudo docker run -itd pmallozzi/rl_monitor Xvfb :1 -screen 0 800x600x16
 sudo docker exec -it $(sudo docker ps -lq) git pull origin gym_torcs
 sudo docker exec -itd $(sudo docker ps -lq) x11vnc -forever -create -display :1.0
 
-if [ -z $fn ]
-then
-	sudo docker exec -itd $(sudo docker ps -lq) make -C monitor/reward_3_speed/ compile
-	sudo docker exec -itd $(sudo docker ps -lq) make -C monitor/reward_3_speed/ run
-else
-	sudo docker exec -itd $(sudo docker ps -lq) make -C monitor/$fn/ compile
-	sudo docker exec -itd $(sudo docker ps -lq) make -C monitor/$fn/ run
-fi
+sudo docker exec -itd $(sudo docker ps -lq) make -C monitor/$fn/ compile
+sudo docker exec -itd $(sudo docker ps -lq) make -C monitor/$fn/ run
 
-if [[ $detached == "y" ]]
-then
-	sudo docker exec -itd -e "DISPLAY=:1.0" $(sudo docker ps -lq) python ddpg.py
-else
-	sudo docker exec -it -e "DISPLAY=:1.0" $(sudo docker ps -lq) python ddpg.py
-fi
+sudo docker exec -it $d -e "DISPLAY=:1.0" $(sudo docker ps -lq) python ddpg.py $monitor
