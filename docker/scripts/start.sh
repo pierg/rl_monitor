981320ac177a028f391959ff0aedadae51347176
PoissonSoluble <vincent.duplessis14@gmail.com>
2017-07-06 11:58:12 +0200

    start script modified with parser

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -27,6 +27,9 @@ done
 sudo docker pull pmallozzi/rl_monitor
 sudo docker run -itd pmallozzi/rl_monitor Xvfb :1 -screen 0 800x600x16
 sudo docker exec -it $(sudo docker ps -lq) git pull origin gym_torcs
+sudo docker exec -it $(sudo docker ps -lq) cd .. && git clone https://github.com/pierg/uppaal_to_larva.git
+sudo docker exec -it $(sudo docker ps -lq) mkdir /rl_monitor/monitor/$fn
+sudo docker exec -it $(sudo docker ps -lq) cd ../uppaal_to_larva && java -jar uppaal_to_larva.jar /rl_monitor/monitor/uppaal_models/$fn.xml /rl_monitor/monitor/$fn/
 sudo docker exec -itd $(sudo docker ps -lq) x11vnc -forever -create -display :1.0
 
 sudo docker exec -itd $(sudo docker ps -lq) make -C monitor/$fn/ compile
