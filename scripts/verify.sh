ce8d534939f3286f6388a00c5aaabd1c54ba912e
Piergiuseppe <mallozzi@chalmers.se>
2017-08-16 18:50:20 +0200

    added verify script

---
+++ scripts/verify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'No arguments provided'
+    echo 'Provide: '
+    echo '	uppaal_model.xml'
+    exit 1
+fi
+
+sh ../monitor/verification/verify_all_properties.sh ../monitor/uppaal_models/$1
