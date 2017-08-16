709dae406b98c6d41f58c0054363d70356b84bb7
Piergiuseppe <mallozzi@chalmers.se>
2017-08-16 17:06:40 +0200

    fixed results script

--- scripts/counters.sh
+++ scripts/counters.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #sudo matlab -nojvm -r "run RUN_counters.m"
 
-
-for D in `find ~/results_tf_torcs -type d`
+for D in `find ~/results_tf_torcs/* -type d`
 do
-    sudo matlab -nojvm -r "run ~/results_tf_torcs/${D}/RUN_counters.m"
+    sudo matlab -nojvm -r "${D}/RUN_counters.m"
 done
