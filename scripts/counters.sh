76adf9fb8d03af17dcbed44fa38d088f8932c59e
Piergiuseppe <mallozzi@chalmers.se>
2017-08-16 17:01:16 +0200

    fixed results script

--- scripts/counters.sh
+++ scripts/counters.sh
@@ -2,8 +2,7 @@
 #sudo matlab -nojvm -r "run RUN_counters.m"
 
 
-for D in `find ~/results_tf_torcs -type d` do
-    if [ -d "${D}" ]; then
-        sudo matlab -nojvm -r "run ~/results_tf_torcs/${D}/RUN_counters.m"
-    fi
+for D in `find ~/results_tf_torcs -type d`
+do
+    sudo matlab -nojvm -r "run ~/results_tf_torcs/${D}/RUN_counters.m"
 done
--- scripts/plot_all.sh
+++ scripts/plot_all.sh
@@ -2,8 +2,7 @@
 #sudo matlab -nojvm -r "run RUN_full.m"
 
 
-for D in `find ~/results_tf_torcs -type d` do
-    if [ -d "${D}" ]; then
-        sudo matlab -nojvm -r "run ~/results_tf_torcs/${D}/RUN_full.m"
-    fi
+for D in `find ~/results_tf_torcs -type d`
+do
+    sudo matlab -nojvm -r "run ~/results_tf_torcs/${D}/RUN_full.m"
 done
