d14342d7d34073a7cb0a797bbec7f423e5b75a15
Piergiuseppe <mallozzi@chalmers.se>
2017-08-16 16:58:28 +0200

    fixed results script

--- scripts/plot_all.sh
+++ scripts/plot_all.sh
@@ -2,7 +2,7 @@
 #sudo matlab -nojvm -r "run RUN_full.m"
 
 
-for D in ~/results_tf_torcs; do
+for D in `find ~/results_tf_torcs -type d` do
     if [ -d "${D}" ]; then
         sudo matlab -nojvm -r "run ~/results_tf_torcs/${D}/RUN_full.m"
     fi
