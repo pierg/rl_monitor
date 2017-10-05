65dc3edfdc20162c9b18f2ac2ac7ad4c6be23141
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2017-10-05 17:27:25 +0200

    Update SCRIPT_torcsvalues.m

--- matlab_scripts/SCRIPT_torcsvalues.m
+++ matlab_scripts/SCRIPT_torcsvalues.m
@@ -9,6 +9,8 @@ mkdir torcs;
   set(fig_torcs,'visible','off');
   set(fig_torcs, 'PaperPositionMode', 'manual');
   set(fig_torcs, 'PaperPosition', [0 0 200 50]);
+  
+  
   grid('on');
   ALL_trackPos = cell2mat(trackPos{1, i});
   subplot(6,1,1);
@@ -16,26 +18,35 @@ mkdir torcs;
   hold on;
   xlabel('Step #');
   ylabel('TrackPos');
-  
-  grid('on');
-  ALL_steering = cell2mat(steering{1,i});
-  subplot(6,1,2);
-  plot(ALL_steering);
-  hold on;
-  xlabel('Step #');
-  ylabel('Steering');
-  
+ 
   grid('on');
   ALL_speedX = cell2mat(speedX{1,i});
-  subplot(6,1,3);
+  subplot(6,1,2);
   plot(ALL_speedX);
   hold on;
   xlabel('Step #');
   ylabel('SpeedX');
   
   grid('on');
-  ALL_accel = cell2mat(accel{1,i});
+  ALL_damage = cell2mat(damage{1,i});
+  subplot(6,1,3);
+  plot(ALL_damage);
+  hold on;
+  xlabel('Step #');
+  ylabel('Damage');
+  
+  
+  grid('on');
+  ALL_steering = cell2mat(steering{1,i});
   subplot(6,1,4);
+  plot(ALL_steering);
+  hold on;
+  xlabel('Step #');
+  ylabel('Steering');
+  
+  grid('on');
+  ALL_accel = cell2mat(accel{1,i});
+  subplot(6,1,5);
   plot(ALL_accel);
   hold on;
   xlabel('Step #');
@@ -43,19 +54,12 @@ mkdir torcs;
   
   grid('on');
   ALL_brake = cell2mat(brake{1,i});
-  subplot(6,1,5);
+  subplot(6,1,6);
   plot(ALL_brake);
   hold on;
   xlabel('Step #');
   ylabel('Brake');
   
-  grid('on');
-  ALL_damage = cell2mat(damage{1,i});
-  subplot(6,1,6);
-  plot(ALL_damage);
-  hold on;
-  xlabel('Step #');
-  ylabel('Damage');
   
   file_name = ['torcs/' num2str(i) '_TORCS_VALUESeps'];
   
